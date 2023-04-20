42ccc70e82e6d161f6aa198c10b84403c8da65ec
Ngo Van Chien <chienngo03568@gmail.com>
2023-04-20 21:06:11 +0700

    Render Element

--- ReactMainConcept/my-app/src/index.js
+++ ReactMainConcept/my-app/src/index.js
@@ -1,14 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// bai 3
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
+// import Welcome from "./Welcome";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// const element = (
+//   <div>
+//     <Welcome name="Sara" age={26} />
+//     <Welcome name="Helen" age={27} />
+//     <Welcome name="Alex" age={28} />
+//   </div>
+// );
+// root.render(element);
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+// CHUONG 10, bài 1,2
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+// const element = <h1>Hello World!</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
