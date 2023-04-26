51db14a8cee22f0ecf35ad479cfe23f90b3b8b72
Ngo Van Chien <chienngo03568@gmail.com>
2023-04-26 15:11:52 +0700

    React Hook

---
+++ ReactHook/my-app/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import UserClassComponent from "./User.class";
+import User from "./User";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <UserClassComponent />ss */}
+      <User />
+    </div>
+  );
+}
+
+export default App;
