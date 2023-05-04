91a07b4fc24362299eb85ac2af0516017b943e89
Ngo Van Chien <chienngo03568@gmail.com>
2023-05-04 21:56:54 +0700

    useEffect

---
+++ ReactHook/my-app/src/Header/Navigator.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import useUser from "../useUser";
+
+export default function Navigator() {
+  //   const [user, setUser] = useState({});
+
+  //   useEffect(() => {
+  //     getUser().then((res) => {
+  //       setUser(res.data);
+  //     });
+  //   }, []);
+
+  const user = useUser({});
+
+  return <div>Navigator {user?.name}</div>;
+}
