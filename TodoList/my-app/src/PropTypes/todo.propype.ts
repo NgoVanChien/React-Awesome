26905e0c733a806bec1b14c65daf12f401570bf9
Ngo Van Chien <chienngo03568@gmail.com>
2023-05-05 22:15:53 +0700

    Edit proptype file

--- TodoList/my-app/src/PropTypes/todo.propype.ts
+++ TodoList/my-app/src/PropTypes/todo.propype.ts
@@ -3,5 +3,5 @@ import PropTypes from 'prop-types'
 export const TodoTypes = PropTypes.shape({
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  done: PropTypes.bool
-})
+  done: PropTypes.bool.isRequired
+}).isRequired
