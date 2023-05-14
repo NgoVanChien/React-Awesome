8232064ebbbc5899c42a1bb8983fd8485d349229
Ngo Van Chien <chienngo03568@gmail.com>
2023-05-14 12:59:46 +0700

    Cách dùng reducer với "MapObject" trong createReducer

--- Redux/old-local-blog/src/pages/blog/blog.reducer.ts
+++ Redux/old-local-blog/src/pages/blog/blog.reducer.ts
@@ -1,4 +1,4 @@
-import { createAction, createReducer } from '@reduxjs/toolkit'
+import { PayloadAction, createAction, createReducer, current } from '@reduxjs/toolkit'
 import { initialPostList } from 'constants/blog'
 import { Post } from 'types/blog.type'
 
@@ -18,40 +18,94 @@ export const startEditingPost = createAction<string>('/blog/startEditingPost')
 export const cancelEditingPost = createAction('/blog/cancelEditingPost')
 export const finishEditingPost = createAction<Post>('/blog/finishEditingPost')
 
-const blogReducer = createReducer(initialState, (buider) => {
-  buider
-    .addCase(addPost, (state, action) => {
-      // immmerjs
-      // immerrjs giúp mutate 1 state an toàn
-      const post = action.payload
-      state.postList.push(post)
-    })
-    .addCase(deletePost, (state, action) => {
-      const postId = action.payload
-      const foundPostIndex = state.postList.findIndex((post) => post.id === postId)
-      if (foundPostIndex !== -1) {
-        state.postList.splice(foundPostIndex, 1)
+// const blogReducer = createReducer(initialState, (buider) => {
+//   buider
+//     .addCase(addPost, (state, action) => {
+//       // immmerjs
+//       // immerrjs giúp mutate 1 state an toàn
+//       const post = action.payload
+//       state.postList.push(post)
+//     })
+//     .addCase(deletePost, (state, action) => {
+//       console.log('before delete', current(state))
+
+//       const postId = action.payload
+//       const foundPostIndex = state.postList.findIndex((post) => post.id === postId)
+//       if (foundPostIndex !== -1) {
+//         state.postList.splice(foundPostIndex, 1)
+//       }
+//       console.log('after delete', current(state))
+//     })
+//     .addCase(startEditingPost, (state, action) => {
+//       const postId = action.payload
+//       const foundPost = state.postList.find((post) => post.id === postId) || null
+//       state.editingPost = foundPost
+//     })
+//     .addCase(cancelEditingPost, (state) => {
+//       state.editingPost = null
+//     })
+//     .addCase(finishEditingPost, (state, action) => {
+//       const postId = action.payload.id
+//       state.postList.some((post, index) => {
+//         if (post.id === postId) {
+//           state.postList[index] = action.payload
+//           return true
+//         }
+//         return false
+//       })
+//       state.editingPost = null
+//     })
+//     .addMatcher(
+//       (action) => action.type.includes('cancel'),
+//       (state, action) => {
+//         console.log(current(state))
+//       }
+//     )
+//   // // addMatcher cho phép chúng ta thêm "matcher function"
+//   //   // nếu "matcher function" return true thì nó sẽ nhảy vào case này
+//   //   .addMatcher(isActionWithNumberPayload, (state, action) => {})
+//   //   // nếu muốn thêm default case khi không match case nào cả
+//   //   // thì dùng addDefaultCase
+//   //   >>.addDefaultCase((state, action) => {});
+// })
+
+const blogReducer = createReducer(initialState, {
+  [addPost.type]: (state, action: PayloadAction<Post>) => {
+    // immmerjs
+    // immerrjs giúp mutate 1 state an toàn
+    const post = action.payload
+    state.postList.push(post)
+  },
+  [deletePost.type]: (state, action) => {
+    console.log('before delete', current(state))
+
+    const postId = action.payload
+    const foundPostIndex = state.postList.findIndex((post) => post.id === postId)
+    if (foundPostIndex !== -1) {
+      state.postList.splice(foundPostIndex, 1)
+    }
+    console.log('after delete', current(state))
+  },
+  [startEditingPost.type]: (state, action) => {
+    const postId = action.payload
+    const foundPost = state.postList.find((post) => post.id === postId) || null
+    state.editingPost = foundPost
+  },
+  [cancelEditingPost.type]: (state) => {
+    state.editingPost = null
+  },
+
+  [finishEditingPost.type]: (state, action) => {
+    const postId = action.payload.id
+    state.postList.some((post, index) => {
+      if (post.id === postId) {
+        state.postList[index] = action.payload
+        return true
       }
+      return false
     })
-    .addCase(startEditingPost, (state, action) => {
-      const postId = action.payload
-      const foundPost = state.postList.find((post) => post.id === postId) || null
-      state.editingPost = foundPost
-    })
-    .addCase(cancelEditingPost, (state) => {
-      state.editingPost = null
-    })
-    .addCase(finishEditingPost, (state, action) => {
-      const postId = action.payload.id
-      state.postList.some((post, index) => {
-        if (post.id === postId) {
-          state.postList[index] = action.payload
-          return true
-        }
-        return false
-      })
-      state.editingPost = null
-    })
+    state.editingPost = null
+  }
 })
 
 export default blogReducer
