13e37ad07e32efffbfd203bacafe98449f05cf17
Ngo Van Chien <chienngo03568@gmail.com>
2023-05-24 00:06:17 +0700

    Nested Route

--- ReactRouter/learn-react-router/src/App.tsx
+++ ReactRouter/learn-react-router/src/App.tsx
@@ -1,9 +1,12 @@
 import AddStaff from 'components/AddStaff'
 import StaffItem from 'components/StaffItem'
+import StaffList from 'components/StaffList'
 import MainLayout from 'layouts/MainLayout'
 import About from 'pages/About'
 import Dashboard from 'pages/Dashboard'
-import StaffList from 'pages/StaffList'
+import NotFound from 'pages/NotFound'
+import Staff from 'pages/Staff'
+// import StaffList from 'pages/Staff'
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -11,15 +14,19 @@ function App() {
     <div className='App'>
       <MainLayout>
         <Routes>
-          <Route path='/' element={<Dashboard />}></Route>
-          <Route path='/About' element={<About />}></Route>
-          <Route path='/Staff' element={<StaffList />}></Route>
-          <Route path='/Staff/:id' element={<StaffItem />}></Route>
-          <Route path='/Staff/add' element={<AddStaff />}></Route>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/About' element={<About />} />
+          <Route path='/Staff' element={<Staff />}>
+            <Route path=':id' element={<StaffItem />} />
+            <Route path='add' element={<AddStaff />} />
+            <Route path='list' element={<StaffList />} />
+          </Route>
+
+          {/* <Route path='/Staff' element={<Staff />} />
+          <Route path='/Staff/:id' element={<StaffItem />} />
+          <Route path='/Staff/add' element={<AddStaff />} /> */}
+          <Route path='/*' element={<NotFound />} />
         </Routes>
-        {/* <Dashboard />
-        <About />
-        <StaffList /> */}
       </MainLayout>
     </div>
   )
--- ReactRouter/learn-react-router/src/components/StaffItem/StaffItem.tsx
+++ ReactRouter/learn-react-router/src/components/StaffItem/StaffItem.tsx
@@ -1,8 +1,10 @@
-import { useParams } from 'react-router-dom'
+import { useParams, useOutletContext } from 'react-router-dom'
 
 export default function StaffItem() {
   // const parmams = useParams()
   // console.log(parmams)
+  const context = useOutletContext()
+  console.log(context)
   const { id } = useParams()
   return <div>StaffItem {id}</div>
 }
