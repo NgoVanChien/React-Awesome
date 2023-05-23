14c83bfc8ed5e11fd0e34a512bf40a1e43bf43d6
Ngo Van Chien <chienngo03568@gmail.com>
2023-05-23 18:10:20 +0700

    Dàn Layout, Page và phân chia Component

--- ReactRouter/learn-react-router/src/App.tsx
+++ ReactRouter/learn-react-router/src/App.tsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import logo from './logo.svg'
+import MainLayout from 'layouts/MainLayout'
+import About from 'pages/About'
+import Dashboard from 'pages/Dashboard'
+import StaffList from 'pages/StaffList'
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+      <MainLayout>
+        <Dashboard />
+        <About />
+        <StaffList />
+      </MainLayout>
     </div>
   )
 }
