7980085f0651788e38650a5cdb11a214e69557b0
Ngo Van Chien <chienngo03568@gmail.com>
2023-05-24 00:47:27 +0700

    Chia Route với useRoutes hook thay cho Route

--- ReactRouter/learn-react-router/src/App.tsx
+++ ReactRouter/learn-react-router/src/App.tsx
@@ -4,22 +4,51 @@ import Dashboard from 'pages/Dashboard'
 import NotFound from 'pages/NotFound'
 import Staff from 'pages/Staff'
 // import StaffList from 'pages/Staff'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useRoutes } from 'react-router-dom'
 
 function App() {
+  const elements = useRoutes([
+    {
+      path: '/',
+      element: <Dashboard />
+    },
+    {
+      path: '/about',
+      element: <About />
+      //   children: [
+      //     {
+      //       path: ,
+      //       element:
+      //     }
+      // ]
+    },
+    {
+      path: '/staff',
+      element: <Staff />
+    },
+    {
+      path: '/staff/*',
+      element: <Staff />
+    },
+    {
+      path: '/*',
+      element: <NotFound />
+    }
+  ])
   return (
     <div className='App'>
       <MainLayout>
-        <Routes>
+        {elements}
+        {/* <Routes>
           <Route path='/' element={<Dashboard />} />
-          <Route path='/About' element={<About />} />
-          <Route path='/Staff/*' element={<Staff />}></Route>
+          <Route path='/about' element={<About />} />
+          <Route path='/staff/*' element={<Staff />}></Route> */}
 
-          {/* <Route path='/Staff' element={<Staff />} />
+        {/* <Route path='/Staff' element={<Staff />} />
           <Route path='/Staff/:id' element={<StaffItem />} />
           <Route path='/Staff/add' element={<AddStaff />} /> */}
-          <Route path='/*' element={<NotFound />} />
-        </Routes>
+        {/* <Route path='/*' element={<NotFound />} />
+        </Routes> */}
       </MainLayout>
     </div>
   )
