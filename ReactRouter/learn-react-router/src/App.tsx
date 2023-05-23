425eb50e8138ac9b16f5e00f56ce2e39f967a6f5
Ngo Van Chien <chienngo03568@gmail.com>
2023-05-23 20:47:21 +0700

    Chia Router cơ bản

--- ReactRouter/learn-react-router/src/App.tsx
+++ ReactRouter/learn-react-router/src/App.tsx
@@ -2,14 +2,20 @@ import MainLayout from 'layouts/MainLayout'
 import About from 'pages/About'
 import Dashboard from 'pages/Dashboard'
 import StaffList from 'pages/StaffList'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className='App'>
       <MainLayout>
-        <Dashboard />
+        <Routes>
+          <Route path='/' element={<Dashboard />}></Route>
+          <Route path='/About' element={<About />}></Route>
+          <Route path='/Staff' element={<StaffList />}></Route>
+        </Routes>
+        {/* <Dashboard />
         <About />
-        <StaffList />
+        <StaffList /> */}
       </MainLayout>
     </div>
   )
