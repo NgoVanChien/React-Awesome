31139a7abab59bbaca76b9f89f7ea72126c7bafb
Ngo Van Chien <chienngo03568@gmail.com>
2023-05-25 00:23:42 +0700

    Cài devtools cho ReactQuery

--- ReactQuery/student-management/src/index.tsx
+++ ReactQuery/student-management/src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 // Create a client
 const queryClient = new QueryClient()
 
@@ -16,7 +16,9 @@ root.render(
     <BrowserRouter>
       {/* // Provide the client to your App */}
       <QueryClientProvider client={queryClient}>
+        {/* The rest of your application */}
         <App />
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
